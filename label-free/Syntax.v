a7aceefda2499d8c9e50f8680133884f042e2b84
Agata M <ayertienna@gmail.com>
2012-05-22 15:12:25 +0200

    Label-free language, locally closed terms

--- label-free/Syntax.v
+++ label-free/Syntax.v
@@ -1,4 +1,5 @@
 Require Import Metatheory.
+Require Import List.
 
 Inductive ty_LF :=
 | tvar_LF: ty_LF
@@ -68,5 +69,103 @@ apply eq_ctx_LF_dec.
 apply eq_ctx_LF_dec.
 apply eq_ctx_LF_dec.
 Qed.
+
+(* Calculate list of free worlds used in term M *)
+Fixpoint free_worlds_LF (M: te_LF) : fset var :=
+match M with
+| hyp_LF _ => \{}
+| lam_LF _ M => free_worlds_LF M
+| appl_LF M N => free_worlds_LF M \u free_worlds_LF N
+| box_LF M => free_worlds_LF M
+| unbox_LF M => free_worlds_LF M
+| unbox_fetch_LF (fctx w) M => \{w} \u free_worlds_LF M
+| unbox_fetch_LF _ M => free_worlds_LF M
+| here_LF M => free_worlds_LF M
+| get_here_LF (fctx w) M => \{w} \u free_worlds_LF M
+| get_here_LF _ M => free_worlds_LF M
+| letdia_LF M N => free_worlds_LF M \u free_worlds_LF N
+| letdia_get_LF (fctx w) M N => \{w} \u free_worlds_LF M \u free_worlds_LF N
+| letdia_get_LF _ M N => free_worlds_LF M \u free_worlds_LF N
+end.
+
+Definition fresh_world_LF (w: var) (M: te_LF) := w \notin (free_worlds_LF M).
+
+(* When a term is locally closed at level n *)
+Inductive lc_w_n_LF : te_LF -> nat -> Prop :=
+ | lcw_hyp_LF: forall x n, lc_w_n_LF (hyp_LF x) n
+ | lcw_lam_LF: forall t M n, lc_w_n_LF M n -> lc_w_n_LF (lam_LF t M) n
+ | lcw_appl_LF: forall M N n, lc_w_n_LF M n -> lc_w_n_LF N n -> lc_w_n_LF (appl_LF M N) n
+ | lcw_box_LF: forall M n, lc_w_n_LF M (S n) -> lc_w_n_LF (box_LF M) n
+ | lcw_unbox_LF: forall M n, lc_w_n_LF M n -> lc_w_n_LF (unbox_LF M) n
+ | lcw_unbox_fetch_LF: forall M n w, lc_w_n_LF M n -> 
+   lc_w_n_LF (unbox_fetch_LF (fctx w) M) n
+ | lcw_here_LF: forall M n, lc_w_n_LF M n -> lc_w_n_LF (here_LF M) n
+ | lcw_get_here_LF: forall M n w , lc_w_n_LF M n -> lc_w_n_LF (get_here_LF (fctx w) M) n
+ | lcw_letdia_LF: forall M N n, lc_w_n_LF N (S n) -> lc_w_n_LF M n -> 
+   lc_w_n_LF (letdia_LF M N) n
+ | lcw_letdia_get_LF: forall M N n w, lc_w_n_LF N (S n) -> lc_w_n_LF M n -> 
+   lc_w_n_LF (letdia_get_LF (fctx w) M N) n
+.
+
+(* Calculate list of unbound worlds of level above n *)
+Fixpoint unbound_worlds (n:nat) (M:te_LF):=
+match M with
+| hyp_LF n => nil
+| lam_LF t M => unbound_worlds n M
+| appl_LF M N => unbound_worlds n M ++ unbound_worlds n N
+| box_LF M => unbound_worlds (S n) M
+| unbox_LF M => unbound_worlds n M
+| unbox_fetch_LF (bctx w) M => w :: unbound_worlds n M
+| unbox_fetch_LF (fctx w) M => unbound_worlds n M
+| here_LF M => unbound_worlds n M
+| get_here_LF (bctx w) M => w :: unbound_worlds n M
+| get_here_LF (fctx w) M => unbound_worlds n M
+| letdia_LF M N => unbound_worlds n M ++ unbound_worlds (S n) N
+| letdia_get_LF (bctx w) M N => w :: unbound_worlds n M ++ unbound_worlds (S n) N
+| letdia_get_LF (fctx w) M N => unbound_worlds n M ++ unbound_worlds (S n) N
+end.
+
+Definition lc_w_LF (t:te_LF) : Prop := lc_w_n_LF t 0.
+
+Section Lemmas.
+Lemma closed_w_succ:
+forall M n,
+  lc_w_n_LF M n -> lc_w_n_LF M (S n).
+intros; generalize dependent n;
+induction M; intros; inversion H; subst;
+eauto using lc_w_n_LF.
+Qed.
+
+Lemma closed_w_addition:
+forall M n m,
+  lc_w_n_LF M n -> lc_w_n_LF M (n + m).
+intros; induction m.
+replace (n+0) with n by auto; assumption.
+replace (n+ S m) with (S (n+m)) by auto;
+apply closed_w_succ; assumption.
+Qed.
+
+Lemma closed_no_unbound_worlds:
+forall M n,
+  lc_w_n_LF M n -> unbound_worlds n M = nil.
+intros;
+generalize dependent n;
+induction M; intros; simpl in *;
+eauto.
+apply IHM; inversion H; subst; assumption.
+inversion H; subst; rewrite IHM1; try rewrite IHM2; auto.
+apply IHM; inversion H; subst; assumption.
+apply IHM; inversion H; subst; assumption.
+destruct c; [ | apply IHM]; inversion H; subst; auto.
+apply IHM; inversion H; subst; assumption.
+destruct c; [ | apply IHM]; inversion H; subst; auto.
+inversion H; subst; rewrite IHM1; try rewrite IHM2; auto.
+destruct c; inversion H; subst; auto.
+  rewrite IHM1; 
+  [rewrite IHM2 | auto];
+  auto.  
+Qed.
+
+End Lemmas.
 
 Close Scope label_free_is5_scope.
