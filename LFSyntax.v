9c5df19e30b90723124199953fcf76a7d587d119
Agata M <ayertienna@gmail.com>
2012-08-16 00:40:19 +0200

    LFSyntax beautified

--- LFSyntax.v
+++ LFSyntax.v
@@ -1,7 +1,19 @@
 Require Export Shared.
-Require Import LibList.
-Require Import LibListSorted.
+Require Export LibList.
 
+
+(*** Definitions ***)
+
+(*
+Terms in label-free IS5 system:
+  * hypothesis
+  * lambda expression
+  * application
+  * box expression
+  * unpacking box (possibly in a different world)
+  * diamond expression (possibly in a different world)
+  * using diamond expression (possibly diamond is from a different world)
+*)
 Inductive te_LF :=
 | hyp_LF: vte -> te_LF
 | lam_LF: ty -> te_LF -> te_LF
@@ -13,72 +25,54 @@ Inductive te_LF :=
 .
 
 
-(* FIXME: is this going to give us trouble? I.e., should we proove this? *)
-Axiom permut_dec:
-forall (a: list (var * ty)) a',
-  { permut a a' } + { ~permut a a' }.
+(*
+Property: term is locally closed
+ This means that there are no bound variables.
+*)
 
-
-Theorem permut_context_LF_dec:
-forall (c1 c2: Context_LF),
-  { permut (snd c1) (snd c2) /\ (fst c1) = (fst c2)} + 
-  { ~permut (snd c1) (snd c2) \/ (fst c1) <> (fst c2)}.
-intros; destruct c1 as (w, a); destruct c2 as (w', a');
-destruct (eq_var_dec w w'); subst; simpl;
-destruct (permut_dec a a'); simpl;
-auto.
-Qed.
-
-
-(* When a term is context-wise locally closed at level n *)
-Inductive lc_w_n_LF : te_LF -> nat -> Prop :=
- | lcw_hyp_LF: forall v n, lc_w_n_LF (hyp_LF v) n
- | lcw_lam_LF: forall t M n,
-     lc_w_n_LF M n ->
-     lc_w_n_LF (lam_LF t M) n
- | lcw_appl_LF: forall M N n,
-     lc_w_n_LF M n -> lc_w_n_LF N n ->
-     lc_w_n_LF (appl_LF M N) n
- | lcw_box_LF: forall M n,
-     lc_w_n_LF M (S n) ->
-     lc_w_n_LF (box_LF M) n
- | lcw_unbox_fetch_LF: forall M n w,
-     lc_w_n_LF M n ->
-     lc_w_n_LF (unbox_fetch_LF (fwo w) M) n
- | lcw_get_here_LF: forall M n w ,
-     lc_w_n_LF M n ->
-     lc_w_n_LF (get_here_LF (fwo w) M) n
- | lcw_letdia_get_LF: forall M N n w,
-     lc_w_n_LF N (S n) -> lc_w_n_LF M n ->
-     lc_w_n_LF (letdia_get_LF (fwo w) M N) n
+Inductive lc_w_LF : te_LF -> Prop :=
+ | lcw_hyp_LF: forall v, lc_w_LF (hyp_LF v)
+ | lcw_lam_LF: forall t M,
+     lc_w_LF M ->
+     lc_w_LF (lam_LF t M)
+ | lcw_appl_LF: forall M N,
+     lc_w_LF M -> lc_w_LF N ->
+     lc_w_LF (appl_LF M N)
+ | lcw_box_LF: forall M,
+     lc_w_LF M ->
+     lc_w_LF (box_LF M)
+ | lcw_unbox_fetch_LF: forall M w,
+     lc_w_LF M ->
+     lc_w_LF (unbox_fetch_LF (fwo w) M)
+ | lcw_get_here_LF: forall M w,
+     lc_w_LF M ->
+     lc_w_LF (get_here_LF (fwo w) M)
+ | lcw_letdia_get_LF: forall M N w,
+     lc_w_LF N -> lc_w_LF M ->
+     lc_w_LF (letdia_get_LF (fwo w) M N)
 .
-Definition lc_w_LF (t:te_LF) : Prop := lc_w_n_LF t 0.
 
-
-(* When a term is term-wise locally closed at level n *)
-Inductive lc_t_n_LF: te_LF -> nat -> Prop :=
-| lct_hyp_LF: forall v n, lc_t_n_LF (hyp_LF (fte v)) n
-| lct_lam_LF: forall t M n, 
-    lc_t_n_LF M (S n) ->
-    lc_t_n_LF (lam_LF t M) n
-| lct_appl_LF: forall M N n,
-    lc_t_n_LF M n -> lc_t_n_LF N n ->
-    lc_t_n_LF (appl_LF M N) n
- | lct_box_LF: forall M n,
-     lc_t_n_LF M n ->
-     lc_t_n_LF (box_LF M) n
- | lct_unbox_fetch_LF: forall M n w,
-     lc_t_n_LF M n ->
-     lc_t_n_LF (unbox_fetch_LF (fwo w) M) n
- | lct_get_here_LF: forall M n w ,
-     lc_t_n_LF M n ->
-     lc_t_n_LF (get_here_LF (fwo w) M) n
- | lct_letdia_get_LF: forall M N n w,
-     lc_t_n_LF N n -> lc_t_n_LF M n ->
-     lc_t_n_LF (letdia_get_LF (fwo w) M N) n
+Inductive lc_t_LF: te_LF -> Prop :=
+| lct_hyp_LF: forall v, lc_t_LF (hyp_LF (fte v))
+| lct_lam_LF: forall t M,
+    lc_t_LF M ->
+    lc_t_LF (lam_LF t M)
+| lct_appl_LF: forall M N,
+    lc_t_LF M -> lc_t_LF N ->
+    lc_t_LF (appl_LF M N)
+ | lct_box_LF: forall M,
+     lc_t_LF M ->
+     lc_t_LF (box_LF M)
+ | lct_unbox_fetch_LF: forall M w,
+     lc_t_LF M ->
+     lc_t_LF (unbox_fetch_LF (fwo w) M)
+ | lct_get_here_LF: forall M w,
+     lc_t_LF M ->
+     lc_t_LF (get_here_LF (fwo w) M)
+ | lct_letdia_get_LF: forall M N w,
+     lc_t_LF N -> lc_t_LF M ->
+     lc_t_LF (letdia_get_LF (fwo w) M N)
 .
-Definition lc_t_LF (t:te_LF) : Prop := lc_t_n_LF t 0.
-
 
 (* Calculate list of free worlds used in term M *)
 Fixpoint free_worlds_LF (M: te_LF) : fset var :=
@@ -95,24 +89,6 @@ match M with
 | letdia_get_LF _ M N => free_worlds_LF M \u free_worlds_LF N
 end.
 
-
-(* Calculate list of unbound worlds of level above n *)
-Fixpoint unbound_worlds (n:nat) (M:te_LF):=
-match M with
-| hyp_LF n => nil
-| lam_LF t M => unbound_worlds n M
-| appl_LF M N => unbound_worlds n M ++ unbound_worlds n N
-| box_LF M => unbound_worlds (S n) M
-| unbox_fetch_LF (bwo w) M => w :: unbound_worlds n M
-| unbox_fetch_LF (fwo w) M => unbound_worlds n M
-| get_here_LF (bwo w) M => w :: unbound_worlds n M
-| get_here_LF (fwo w) M => unbound_worlds n M
-| letdia_get_LF (bwo w) M N =>
-  w :: unbound_worlds n M ++ unbound_worlds (S n) N
-| letdia_get_LF (fwo w) M N => unbound_worlds n M ++ unbound_worlds (S n) N
-end.
-
-
 (* Calculate list of free variables used in term M *)
 Fixpoint free_vars_LF (M: te_LF) : fset var :=
 match M with
@@ -126,80 +102,67 @@ match M with
 | letdia_get_LF _ M N => free_vars_LF M \u free_vars_LF N
 end.
 
+(* Calculate list of bound worlds in the term *)
+Fixpoint bound_worlds (M: te_LF) :=
+match M with
+| hyp_LF n => nil
+| lam_LF t M => bound_worlds M
+| appl_LF M N => bound_worlds M ++ bound_worlds N
+| box_LF M => bound_worlds M
+| unbox_fetch_LF (bwo w) M => w :: bound_worlds M
+| unbox_fetch_LF (fwo w) M => bound_worlds M
+| get_here_LF (bwo w) M => w :: bound_worlds M
+| get_here_LF (fwo w) M => bound_worlds M
+| letdia_get_LF (bwo w) M N => w :: bound_worlds M ++ bound_worlds N
+| letdia_get_LF (fwo w) M N => bound_worlds M ++ bound_worlds N
+end.
 
-(* Calculate list of unbound variables of level above n *)
-Fixpoint unbound_vars (n:nat) (M:te_LF):=
+(* Calculate list of bound variables of level above n *)
+Fixpoint bound_vars (M: te_LF) :=
 match M with
 | hyp_LF (bte n) => n::nil
-| hyp_LF (fte n) => nil 
-| lam_LF t M => unbound_vars (S n) M
-| appl_LF M N => unbound_vars n M ++ unbound_vars n N
-| box_LF M => unbound_vars n M
-| unbox_fetch_LF _ M => unbound_vars n M
-| get_here_LF _ M => unbound_vars n M
-| letdia_get_LF _ M N => unbound_vars n M ++ unbound_vars (S n) N
+| hyp_LF (fte n) => nil
+| lam_LF t M => bound_vars M
+| appl_LF M N => bound_vars M ++ bound_vars N
+| box_LF M => bound_vars M
+| unbox_fetch_LF _ M => bound_vars M
+| get_here_LF _ M => bound_vars M
+| letdia_get_LF _ M N => bound_vars M ++ bound_vars N
 end.
 
 
-Section Lemmas.
+(*** Properties ***)
 
 
-Lemma closed_w_succ:
-forall M n,
-  lc_w_n_LF M n -> lc_w_n_LF M (S n).
-intros; generalize dependent n;
-induction M; intros; inversion H; subst;
-eauto using lc_w_n_LF.
+(* For each pair of contexts, they are either equivalent or not. *)
+Theorem permut_context_LF_dec:
+forall (c1 c2: Context_LF),
+  {  permut (snd c1) (snd c2) /\ (fst c1) = (fst c2)} +
+  { ~permut (snd c1) (snd c2) \/ (fst c1) <> (fst c2)}.
+intros; destruct c1 as (w, a); destruct c2 as (w', a');
+destruct (eq_var_dec w w'); subst; simpl;
+destruct (permut_dec a a'); simpl;
+auto.
 Qed.
 
-Lemma closed_w_addition:
-forall M n m,
-  lc_w_n_LF M n -> lc_w_n_LF M (n + m).
-intros; induction m;
-[ replace (n+0) with n by auto |
-  replace (n+ S m) with (S (n+m)) by auto] ;
-try apply closed_w_succ;
-assumption.
-Qed.
 
-Lemma closed_w_no_unbound_worlds:
-forall M n,
-  lc_w_n_LF M n -> unbound_worlds n M = nil.
+(* If term is closed at some level, it does not contain bound variables*)
+
+Lemma closed_w_no_bound_worlds:
+forall M,
+  lc_w_LF M -> bound_worlds M = nil.
 intros;
-generalize dependent n;
 induction M; intros; simpl in *;
 inversion H; subst; eauto;
-rewrite IHM1; try rewrite IHM2; auto.
+erewrite IHM1; try erewrite IHM2; eauto.
 Qed.
 
-Lemma closed_t_succ:
-forall M n,
-  lc_t_n_LF M n -> lc_t_n_LF M (S n).
-intros; generalize dependent n;
-induction M; intros; inversion H; subst;
-eauto using lc_t_n_LF.
-Qed.
-
-Lemma closed_t_addition:
-forall M n m,
-  lc_t_n_LF M n -> lc_t_n_LF M (n + m).
-intros; induction m;
-[ replace (n+0) with n by auto |
-  replace (n + S m) with (S (n+m)) by auto] ;
-try apply closed_t_succ;
-assumption.
-Qed.
-
-Lemma closed_t_no_unbound_vars:
-forall M n,
-  lc_t_n_LF M n -> unbound_vars n M = nil.
+Lemma closed_t_no_bound_vars:
+forall M,
+  lc_t_LF M -> bound_vars M = nil.
 intros;
-generalize dependent n;
 induction M; intros; simpl in *;
 inversion H; subst; eauto;
-rewrite IHM1; try rewrite IHM2; auto;
+erewrite IHM1; try erewrite IHM2; eauto;
 apply closed_t_succ; assumption.
 Qed.
-
-End Lemmas.
-
