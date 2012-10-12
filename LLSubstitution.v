aeef76ba164fa1a08eb81e7e22f2ae8c96f069a8
Agata M <ayertienna@gmail.com>
2012-10-12 16:59:16 +0200

    Small fix for lc_* in Labeled

--- LLSubstitution.v
+++ LLSubstitution.v
@@ -1,6 +1,4 @@
 Require Export LLSyntax.
-Require Export LibLogic.
-Require Export LibTactics.
 
 (* Notation for term substitution *)
 Global Reserved Notation " [ M // v ] N " (at level 5).
@@ -39,10 +37,10 @@ match M0 with
 | box_L M => box_L ({{ shift_vwo w1 // shift_vwo w2 }} M)
 | unbox_L M => unbox_L ({{w1//w2}}M)
 | fetch_L w3 M =>
-  let w' := If w3 = w2 then w1 else w3 in
+  let w' := if (eq_vwo_dec w3 w2) then w1 else w3 in
     fetch_L w' ({{w1//w2}} M)
 | get_L w3 M =>
-  let w' := If w3 = w2 then w1 else w3 in
+  let w' := if (eq_vwo_dec w3 w2) then w1 else w3 in
     get_L w' ({{w1//w2}}M)
 | here_L M => here_L ({{w1//w2}}M)
 | letd_L M N =>
@@ -56,62 +54,3 @@ Definition open_w M w := subst_w M w (bwo 0).
 
 Notation " M '^t^' t " := (open_t M t) (at level 5) : labeled_is5_scope.
 Notation " M ^w^ w  " := (open_w M w) (at level 10) : labeled_is5_scope.
-
-Open Scope labeled_is5_scope.
-
-Inductive lc_t_L : te_L -> Prop :=
- | lct_hyp_L: forall v, lc_t_L (hyp_L (fte v))
- | lct_lam_L: forall L t M,
-     forall x, x \notin L -> lc_t_L (M ^t^ (hyp_L (fte x))) ->
-     lc_t_L (lam_L t M)
- | lct_appl_L: forall M N,
-     lc_t_L M -> lc_t_L N ->
-     lc_t_L (appl_L M N)
- | lct_box_L: forall M,
-     lc_t_L M ->
-     lc_t_L (box_L M)
- | lct_unbox_L: forall M,
-     lc_t_L M ->
-     lc_t_L (unbox_L M)
- | lct_fetch_L: forall M w,
-     lc_t_L M ->
-     lc_t_L (fetch_L w M)
- | lct_here_L: forall M,
-     lc_t_L M ->
-     lc_t_L (here_L M)
- | lct_get_L: forall M w,
-     lc_t_L M ->
-     lc_t_L (get_L (fwo w) M)
- | lct_letd_L: forall L M N,
-     (forall x, x \notin L -> lc_t_L (N ^t^ (hyp_L (fte x)))) ->
-     lc_t_L M ->
-     lc_t_L (letd_L M N)
-.
-
-Inductive lc_w_L: te_L -> Prop :=
-| lcw_hyp_L: forall v, lc_w_L (hyp_L (fte v))
-| lcw_lam_L: forall t M,
-    lc_w_L M ->
-    lc_w_L (lam_L t M)
-| lcw_appl_L: forall M N,
-    lc_w_L M -> lc_w_L N ->
-    lc_w_L (appl_L M N)
-| lcw_box_L: forall L M,
-    forall w, w \notin L -> lc_w_L (M ^w^ (fwo w)) ->
-    lc_w_L (box_L M)
-| lcw_unbox_L: forall M,
-    lc_w_L M ->
-    lc_w_L (unbox_L M)
-| lcw_fetch_L: forall M w,
-    lc_w_L M ->
-    lc_w_L (fetch_L (fwo w) M)
-| lcw_here_L: forall M,
-    lc_w_L M ->
-    lc_w_L (here_L M)
-| lcw_get_L: forall M w,
-    lc_w_L M ->
-    lc_w_L (get_L (fwo w) M)
-| lcw_letd_L: forall L M N,
-    (forall w', w' \notin L -> lc_w_L (N ^w^ (fwo w'))) -> lc_w_L M ->
-    lc_w_L (letd_L M N)
-.
--- LLSyntax.v
+++ LLSyntax.v
@@ -15,3 +15,60 @@ Inductive te_L :=
 | here_L: te_L -> te_L
 | fetch_L: vwo -> te_L -> te_L
 .
+
+Inductive lc_t_n_L : nat -> te_L -> Prop :=
+ | lct_hyp_fte_L: forall v n, lc_t_n_L n (hyp_L (fte v))
+ | lct_hyp_bte_L: forall v n, n > v -> lc_t_n_L n (hyp_L (bte v))
+ | lct_lam_L: forall t M n,
+     lc_t_n_L (S n) M ->
+     lc_t_n_L n (lam_L t M)
+ | lct_appl_L: forall M N n,
+     lc_t_n_L n M -> lc_t_n_L n N ->
+     lc_t_n_L n (appl_L M N)
+ | lct_box_L: forall M n,
+     lc_t_n_L n M ->
+     lc_t_n_L n (box_L M)
+ | lct_unbox_L: forall M n,
+     lc_t_n_L n M ->
+     lc_t_n_L n (unbox_L M)
+ | lct_fetch_L: forall M w n,
+     lc_t_n_L n M ->
+     lc_t_n_L n (fetch_L w M)
+ | lct_here_L: forall M n,
+     lc_t_n_L n M ->
+     lc_t_n_L n (here_L M)
+ | lct_get_L: forall M w n,
+     lc_t_n_L n M ->
+     lc_t_n_L n (get_L (fwo w) M)
+ | lct_letd_L: forall M N n,
+     lc_t_n_L (S n) N -> lc_t_n_L n M ->
+     lc_t_n_L n (letd_L M N)
+.
+
+Inductive lc_w_n_L: nat -> te_L -> Prop :=
+| lcw_hyp_L: forall v n, lc_w_n_L n (hyp_L v)
+| lcw_lam_L: forall t M n,
+    lc_w_n_L n M ->
+    lc_w_n_L n (lam_L t M)
+| lcw_appl_L: forall M N n,
+    lc_w_n_L n M -> lc_w_n_L n N ->
+    lc_w_n_L n (appl_L M N)
+| lcw_box_L: forall M n,
+    lc_w_n_L (S n) M ->
+    lc_w_n_L n (box_L M)
+| lcw_unbox_L: forall M n,
+    lc_w_n_L n M ->
+    lc_w_n_L n (unbox_L M)
+| lcw_fetch_L: forall M w n,
+    lc_w_n_L n M ->
+    lc_w_n_L n (fetch_L (fwo w) M)
+| lcw_here_L: forall M n,
+    lc_w_n_L n M ->
+    lc_w_n_L n (here_L M)
+| lcw_get_L: forall M w n,
+    lc_w_n_L n M ->
+    lc_w_n_L n (get_L (fwo w) M)
+| lcw_letd_L: forall M N n,
+    lc_w_n_L (S n) N -> lc_w_n_L n M ->
+    lc_w_n_L n (letd_L M N)
+.
