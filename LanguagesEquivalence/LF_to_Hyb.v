b6a94ecf0c382e5428b4d57db8f8e34a09472d8d
Agata M <ayertienna@gmail.com>
2013-03-23 00:59:24 +0100

    LF to Hyb - things breaking all over

--- LanguagesEquivalence/LF_to_Hyb.v
+++ LanguagesEquivalence/LF_to_Hyb.v
@@ -31,15 +31,21 @@ Admitted.
 Add Morphism LF_to_Hyb_ctx: LF_to_Hyb_ctx_LF.
 Admitted.
 
-Inductive LF_to_Hyb_term: var -> te_LF -> te_Hyb -> Prop :=
+(* Problem: we need the rules to reflect types_LF typing trees - otherwise
+how will we tell a difference between in-place unbox and context-changing one?
+we either need to make sure that LF_to_Hyb_term is only between matching
+terms or make a more general relation and state that there always exists
+a term from Hyb that preservesd typing. Or maybe there is a function after all?
+*)
+
+
+Inductive LF_to_Hyb_term: var -> types_LF -> te_LF -> te_Hyb -> Prop :=
+(*
 | hyp_LF_Hyb:
     forall v w, LF_to_Hyb_term w (hyp_LF v) (hyp_Hyb v)
 | lam_LF_Hyb:
-    forall L M N t w,
-      (forall v0, v0 \notin L ->
-                  LF_to_Hyb_term w
-                    (open_LF M (hyp_LF (fte v0)))
-                    (open_t_Hyb N (hyp_Hyb (fte v0)))) ->
+    forall M N t w,
+      LF_to_Hyb_term w M N ->
       LF_to_Hyb_term w (lam_LF t M) (lam_Hyb t N)
 | appl_LF_Hyb:
     forall M M' N N' w,
@@ -51,10 +57,13 @@ Inductive LF_to_Hyb_term: var -> te_LF -> te_Hyb -> Prop :=
       (forall w0, w0 \notin L -> LF_to_Hyb_term w0 M
                                                 (open_w_Hyb N (fwo w0))) ->
       LF_to_Hyb_term w (box_LF M) (box_Hyb N)
+*)
 | unbox_LF_Hyb:
-    forall M N w w',
-      LF_to_Hyb_term w M N ->
-      LF_to_Hyb_term w' (unbox_LF M) (unbox_fetch_Hyb (fwo w) N)
+    forall M N w G Gamma A G',
+      LF_to_Hyb_ctx (Gamma::G) ((w, Gamma)::G') ->
+      LF_to_Hyb_term w (types_LF G Gamma M ([*]A)) M N ->
+      LF_to_Hyb_term w (types_LF G Gamma (unbox_LF M) A)
+                     (unbox_LF M) (unbox_fetch_Hyb (fwo w) N)
 (*
 | here_LF_Hyb:
     forall M N w w',
@@ -69,62 +78,16 @@ Inductive LF_to_Hyb_term: var -> te_LF -> te_Hyb -> Prop :=
 .
 
 Lemma LF_to_Hyb_typing:
-forall M0 G0 Gamma0 A,
-  types_LF G0 Gamma0 M0 A ->
-  forall M w G G0' G',
-  PPermut_LF (Gamma0::G0) G0' ->
-  PPermut_Hyb ((w, Gamma0)::G) G' ->
-  LF_to_Hyb_ctx G0' G' ->
-  LF_to_Hyb_term w M0 M ->
-  G |= (w, Gamma0) |- M ::: A.
-intros M0 G0 Gamma0 A H; induction H; subst;
-intros M1 w G1 G1' G'' Perm1 Perm2 Ctx H2;
-inversion H2; subst.
-(* hyp *)
-econstructor;
-[apply LF_to_Hyb_ctx_Ok in Ctx | ]; auto;
-[rewrite Perm2 | rewrite <- Perm1]; auto.
-(* lam *)
-apply t_lam_Hyb with (L:=L \u L0);
-assert (LF_to_Hyb_ctx G1' G'') by auto;
-apply LF_to_Hyb_ctx_Ok in Ctx; [rewrite Perm2 | rewrite <- Perm1 | |]; auto.
-intros; auto; eapply H0 with (G0:=G1); eauto.
-apply LF_to_Hyb_ctx_extend; rewrite Perm1; rewrite Perm2; auto.
-rewrite <- Perm1; auto.
-(* appl *)
-apply t_appl_Hyb with (A:=A);
-assert (LF_to_Hyb_ctx G1' G'') by auto;
-apply LF_to_Hyb_ctx_Ok in Ctx; auto.
-rewrite Perm2; auto. rewrite <- Perm1; auto.
-eapply IHtypes_LF1; eauto.
-rewrite <- Perm1; auto.
-eapply IHtypes_LF2; eauto.
-rewrite <- Perm1; auto.
-(* box *)
-apply t_box_Hyb with (L:=L).
-apply LF_to_Hyb_ctx_Ok in Ctx; auto.
-assert (G1 & (w, Gamma) ~=~ (w, Gamma) :: G1) by PPermut_Hyb_simpl;
-rewrite H0; rewrite Perm2; auto.
-assert (PPermut_LF (G & Gamma) (Gamma::G)) by PPermut_LF_simpl;
-rewrite H0 in Ok; rewrite <- Perm1; auto.
-intros; eapply IHtypes_LF; eauto.
-skip. (* compatibility relation gives that from Ctx *)
-(* unbox *)
-destruct (eq_var_dec w0 w); subst.
-(* = *)
-constructor; [apply LF_to_Hyb_ctx_Ok in Ctx | eapply IHtypes_LF]; eauto.
-rewrite Perm2; auto.
-rewrite <- Perm1; auto.
-(* <> *)
-assert (exists G0, exists Gamma0, G1 ~=~ (w0, Gamma0)::G0).
-skip.
-destruct H0 as (G0, (Gamma0, H0)).
-rewrite H0.
-apply t_unbox_fetch_Hyb with (Gamma:=Gamma0) (G:=G0).
-skip. (* permut *)
-(* problem: we explicitely use Gamma in the assumptions, so it is required in
-ihtypes :( *)
-eapply IHtypes_LF.
+forall M_LF G_LF Gamma_LF A M_Hyb w G_Hyb,
+  types_LF G_LF Gamma_LF M_LF A ->
+  LF_to_Hyb_term w True M_LF M_Hyb ->
+  LF_to_Hyb_ctx (Gamma_LF :: G_LF) ((w, Gamma_LF)::G_Hyb) ->
+  G_Hyb |= (w, Gamma_LF) |- M_Hyb ::: A.
+intros; generalize dependent w; generalize dependent G_Hyb;
+generalize dependent M_Hyb; induction H;
+intros M_Hyb G_Hyb w T C; intros; inversion T; subst.
+constructor; auto; eapply LF_to_Hyb_ctx_Ok; eauto.
+
 
 Lemma LF_to_Hyb_steps:
 forall M M' N,
