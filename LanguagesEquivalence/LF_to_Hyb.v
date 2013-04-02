88c7152c2eedb23b902f1bc26d73d29d18045edf
Agata M <ayertienna@gmail.com>
2013-04-02 18:00:48 +0200

    Towards LF to Hyb lang. equiv - part next

--- LanguagesEquivalence/LF_to_Hyb.v
+++ LanguagesEquivalence/LF_to_Hyb.v
@@ -22,60 +22,183 @@ forall G G',
   ok_Bg_LF G -> LF_to_Hyb_ctx G G' -> ok_Bg_Hyb G'.
 Admitted.
 
-Lemma LF_to_Hyb_ctx_extend:
+Lemma LF_to_Hyb_ctx_extend_t:
 forall G G' w Gamma v A,
   LF_to_Hyb_ctx (Gamma::G) ((w, Gamma)::G') ->
   LF_to_Hyb_ctx (((v,A)::Gamma)::G) ((w, (v,A)::Gamma)::G').
 Admitted.
 
+Lemma LF_to_Hyb_ctx_extend_w:
+forall G G' w,
+  LF_to_Hyb_ctx G G' ->
+  LF_to_Hyb_ctx (nil::G) ((w, nil)::G').
+Admitted.
+
 Add Morphism LF_to_Hyb_ctx: LF_to_Hyb_ctx_LF.
 Admitted.
 
-(* Problem: we need the rules to reflect types_LF typing trees - otherwise
-how will we tell a difference between in-place unbox and context-changing one?
-we either need to make sure that LF_to_Hyb_term is only between matching
-terms or make a more general relation and state that there always exists
-a term from Hyb that preservesd typing. Or maybe there is a function after all?
-*)
+Fixpoint LF_to_Hyb_ctx_fun (G: bg_LF) (U: list var) :=
+match G with
+| nil => nil
+| Gamma :: G' =>
+  let w:= var_gen (from_list U) in
+  (w, Gamma) :: LF_to_Hyb_ctx_fun G' (w::U)
+end.
 
+Lemma LF_to_Hyb_ctx_Exists:
+forall Gamma G,
+  ok_Bg_LF (Gamma::G) ->
+  exists w' G', LF_to_Hyb_ctx (Gamma::G) ((w', Gamma)::G').
+Admitted.
 
-Inductive LF_to_Hyb_term: var -> types_LF -> te_LF -> te_Hyb -> Prop :=
-(*
+Inductive LF_to_Hyb_rel: vwo -> te_LF -> te_Hyb ->
+                         bg_LF -> ctx_LF -> Prop :=
+
 | hyp_LF_Hyb:
-    forall v w, LF_to_Hyb_term w (hyp_LF v) (hyp_Hyb v)
+    forall G Gamma w v A,
+      types_LF G Gamma (hyp_LF v) A ->
+      LF_to_Hyb_rel w (hyp_LF v) (hyp_Hyb v) G Gamma
+
 | lam_LF_Hyb:
-    forall M N t w,
-      LF_to_Hyb_term w M N ->
-      LF_to_Hyb_term w (lam_LF t M) (lam_Hyb t N)
+    forall L G Gamma w M A B N,
+      types_LF G Gamma (lam_LF A M) (A ---> B) ->
+      (forall v0, v0 \notin L ->
+                  LF_to_Hyb_rel w
+                    (open_LF M (hyp_LF (fte v0)))
+                    (N ^t^ (hyp_Hyb (fte v0))) G ((v0,A)::Gamma)) ->
+      LF_to_Hyb_rel w (lam_LF A M) (lam_Hyb A N) G Gamma
+
 | appl_LF_Hyb:
-    forall M M' N N' w,
-      LF_to_Hyb_term w M M' ->
-      LF_to_Hyb_term w N N' ->
-      LF_to_Hyb_term w (appl_LF M N) (appl_Hyb M' N')
+    forall G Gamma w B M1 M2 N1 N2,
+      types_LF G Gamma (appl_LF M1 N1) B ->
+      LF_to_Hyb_rel w M1 M2 G Gamma ->
+      LF_to_Hyb_rel w N1 N2 G Gamma ->
+      LF_to_Hyb_rel w (appl_LF M1 N1) (appl_Hyb M2 N2) G Gamma
+
 | box_LF_Hyb:
-    forall L M N w,
-      (forall w0, w0 \notin L -> LF_to_Hyb_term w0 M
-                                                (open_w_Hyb N (fwo w0))) ->
-      LF_to_Hyb_term w (box_LF M) (box_Hyb N)
-*)
+    forall L G Gamma w A M N,
+      types_LF G Gamma (box_LF M) ([*]A) ->
+      (forall w0, w0 \notin L ->
+        LF_to_Hyb_rel (fwo w0) M (N ^w^ (fwo w0)) (G & Gamma) nil) ->
+      LF_to_Hyb_rel w (box_LF M) (box_Hyb N) G Gamma
+
 | unbox_LF_Hyb:
-    forall M N w G Gamma A G',
-      LF_to_Hyb_ctx (Gamma::G) ((w, Gamma)::G') ->
-      LF_to_Hyb_term w (types_LF G Gamma M ([*]A)) M N ->
-      LF_to_Hyb_term w (types_LF G Gamma (unbox_LF M) A)
-                     (unbox_LF M) (unbox_fetch_Hyb (fwo w) N)
+    forall G Gamma w A M N,
+      types_LF G Gamma M ([*]A) ->
+      LF_to_Hyb_rel w M N G Gamma ->
+      LF_to_Hyb_rel w (unbox_LF M) (unbox_fetch_Hyb w N) G Gamma
+
+| unbox_fetch_LF_Hyb:
+    forall G Gamma Gamma' w w' A M N,
+      w <> w' ->
+      types_LF (Gamma::G) Gamma' M ([*]A) ->
+      LF_to_Hyb_rel w' M N (Gamma::G) Gamma' ->
+      LF_to_Hyb_rel w (unbox_LF M) (unbox_fetch_Hyb w' N) (Gamma'::G) Gamma
 (*
 | here_LF_Hyb:
-    forall M N w w',
-      LF_to_Hyb_term w M N ->
-      LF_to_Hyb_term w' (here_LF M) (get_here_Hyb w N)
+    forall G Gamma w A M N,
+      types_LF G Gamma (here_LF M) (<*>A) ->
+      LF_to_Hyb_rel w M N ->
+      LF_to_Hyb_rel w (here_LF M) (get_here_Hyb w N)
+
+| get_here_LF_Hyb:
+    forall G Gamma Gamma' w w' A M N,
+      w <> w' ->
+      types_LF (Gamma'::G) Gamma (here_LF M) (<*>A) ->
+      LF_to_Hyb_rel w' M N ->
+      LF_to_Hyb_rel w (here_LF M) (get_here_Hyb w' N)
+
 | letdia_LF_Hyb:
-    forall M M' N N' w w',
-      LF_to_Hyb_term w M M' ->
-      LF_to_Hyb_term w' N N' ->
-      LF_to_Hyb_term w' (letdia_LF M N) (letdia_get_Hyb w M' N')
+    forall G Gamma w B M1 M2 N1 N2,
+      types_LF G Gamma (letdia_LF M1 N1) B ->
+      LF_to_Hyb_rel w M1 M2 ->
+      LF_to_Hyb_rel w N1 N2 ->
+      LF_to_Hyb_rel w (letdia_LF M1 N1) (letdia_get_Hyb w M2 N2)
+
+| letdia_get_LF_Hyb:
+    forall G Gamma Gamma' w w' B M1 M2 N1 N2,
+      w <> w' ->
+      types_LF (Gamma'::G) Gamma (letdia_LF M1 N1) B ->
+      LF_to_Hyb_rel w' M1 M2 ->
+      LF_to_Hyb_rel w N1 N2 ->
+      LF_to_Hyb_rel w (letdia_LF M1 N1) (letdia_get_Hyb w' M2 N2)
 *)
 .
+
+Lemma LF_to_Hyb_types:
+forall M G Gamma A G' w M',
+  types_LF G Gamma M A ->
+  LF_to_Hyb_rel (fwo w) M M' G Gamma->
+  LF_to_Hyb_ctx (Gamma::G) ((w, Gamma)::G') ->
+  G' |= (w, Gamma) |- M' ::: A.
+intros; generalize dependent G'; generalize dependent M';
+generalize dependent w; induction H; intros.
+(* hyp *)
+inversion H0; subst;
+constructor; [eapply LF_to_Hyb_ctx_Ok | ]; eauto.
+(* lam *)
+inversion H1; subst;
+apply t_lam_Hyb with (L:=L \u L0);
+[eapply LF_to_Hyb_ctx_Ok |
+ intros; unfold open_t_Hyb in *; unfold open_LF in *]; eauto;
+eapply H0; eauto;
+apply LF_to_Hyb_ctx_extend_t; eauto.
+(* appl *)
+inversion H1; subst; econstructor;
+[eapply LF_to_Hyb_ctx_Ok | |]; eauto.
+(* box *)
+inversion H0; subst;
+assert (PPermut_Hyb (G' & (w, Gamma)) ((w, Gamma) :: G'))
+  as Eq1 by PPermut_Hyb_simpl;
+assert (PPermut_LF (G & Gamma) (Gamma :: G))
+  as Eq2 by PPermut_LF_simpl;
+econstructor; [rewrite Eq1; eapply LF_to_Hyb_ctx_Ok | ]; eauto;
+[rewrite Eq2; auto | intros; unfold open_w_Hyb in *];
+eapply IHtypes_LF; eauto;
+eapply LF_to_Hyb_ctx_extend_w; rewrite Eq1; rewrite Eq2; eauto.
+(* unbox *)
+inversion H0; subst.
+constructor; [eapply LF_to_Hyb_ctx_Ok | ]; eauto.
+clear H5 H4 A0.
+
+inversion H0; subst; [elim H3; auto | ].
+destruct w'. skip.
+assert (exists G0, G' ~=~ (v, Gamma') :: G0) by skip.
+destruct H2 as (G'', H2); rewrite H2.
+apply t_unbox_fetch_Hyb with (G:=G'') (Gamma:=Gamma').
+skip.
+eapply IHtypes_LF.
+(* unbox_fetch *)
+(* here *)
+(* letdia *)
+
+Admitted.
+
+Lemma LF_to_Hyb_term:
+forall G Gamma M A,
+  types_LF G Gamma M A ->
+  exists G' w M',
+    LF_to_Hyb_typing G Gamma M G' (w, Gamma) M' A.
+intros; induction H.
+assert (ok_Bg_LF (Gamma::G)) by auto;
+apply LF_to_Hyb_ctx_Exists in Ok; destruct Ok as (w', (G', Ok)).
+exists G'; exists w'; exists (hyp_Hyb (fte v)).
+constructor; destruct Ok; rew_map in *; simpl in *.
+skip. constructor; auto.
+Admitted.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Lemma LF_to_Hyb_typing:
 forall M_LF G_LF Gamma_LF A M_Hyb w G_Hyb,
