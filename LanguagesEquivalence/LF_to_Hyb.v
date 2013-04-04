4702d22b67e531dfd254236514357eff468b2259
Agata M <ayertienna@gmail.com>
2013-04-04 23:31:45 +0200

    LF to Hyb: testing

--- LanguagesEquivalence/LF_to_Hyb.v
+++ LanguagesEquivalence/LF_to_Hyb.v
@@ -270,6 +270,226 @@ rewrite <- H6; eauto.
 PPermut_Hyb_simpl.
 Qed.
 
+Lemma LF_to_Hyb_value:
+forall G' Gamma M A w M',
+  LF_to_Hyb_rel Gamma M A G' w M' ->
+  value_LF M -> value_Hyb M'.
+intros; induction H; simpl;
+inversion H0; subst; constructor; eauto.
+Qed.
+
+(* Things to be moved into language definitions *)
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma lc_w_subst_t_Hyb:
+forall N M v n,
+  lc_w_n_Hyb n M ->
+  lc_w_n_Hyb n N ->
+  lc_w_n_Hyb n (subst_t_Hyb M v N).
+induction N; intros; inversion H0; subst; simpl in *; try case_if;
+auto; constructor; try eapply IHN; eauto. apply closed_w_succ; auto.
+eapply IHN2; [apply closed_w_succ | ]; auto.
+eapply IHN2; [apply closed_w_succ | ]; auto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma lc_w_subst_Hyb:
+forall M w k,
+  lc_w_n_Hyb (S k) M ->
+  lc_w_n_Hyb k {{fwo w // bwo k}} M.
+induction M; intros; simpl in *; repeat case_if;
+inversion H; subst; try destruct w;
+constructor; eauto.
+destruct (eq_nat_dec m k); subst; [elim H0 |]; auto; omega.
+destruct (eq_nat_dec m k); subst; [elim H0 |]; auto; omega.
+destruct (eq_nat_dec m k); subst; [elim H0 |]; auto; omega.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma lc_w_subst_Hyb_same_n_fwo:
+forall M w w' n,
+  lc_w_n_Hyb n M ->
+  lc_w_n_Hyb n {{fwo w // w'}} M.
+induction M; intros; simpl in *; repeat case_if;
+inversion H; subst; try destruct w;
+constructor; eauto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma lc_w_subst_Hyb_same_n_bwo:
+forall M w' k n,
+  lc_w_n_Hyb n M ->
+  k < n ->
+  lc_w_n_Hyb n {{bwo k // w'}} M.
+induction M; intros; simpl in *; repeat case_if;
+inversion H; subst; try destruct w'; simpl;
+constructor; try (eapply IHM || eapply IHM2); try omega; auto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma lc_w_n_Hyb_subst_t:
+forall N M v n,
+lc_w_n_Hyb n (subst_t_Hyb M v N) ->
+lc_w_n_Hyb n N.
+induction N; intros; simpl in *; try destruct v; constructor;
+inversion H; subst; try eapply IHN; eauto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma lc_w_n_Hyb_subst_w:
+forall N w n,
+lc_w_n_Hyb n (subst_w_Hyb (fwo w) (bwo n) N) ->
+lc_w_n_Hyb (S n) N.
+induction N; intros; simpl in *; try destruct v; constructor;
+inversion H; subst; try eapply IHN; eauto;
+repeat case_if; inversion H0; subst; try inversion H1; subst;
+try omega.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Semantics *)
+Lemma types_Hyb_lc_w_Hyb:
+forall G Gamma M A w,
+  G |= (w, Gamma) |- M ::: A -> lc_w_Hyb M.
+intros; induction H; constructor; try apply IHHT;
+unfold open_w_Hyb in *; unfold open_t_Hyb in *;
+auto.
+assert (exists x, x \notin L) by apply Fresh; destruct H0;
+specialize H with x; apply H in H0; apply lc_w_n_Hyb_subst_t in H0; auto.
+assert (exists x, x \notin L) by apply Fresh; destruct H0;
+specialize H with x; apply H in H0; apply lc_w_n_Hyb_subst_w in H0; auto.
+assert (exists x, x \notin L_t) by apply Fresh; destruct H1;
+assert (exists x, x \notin L_w) by apply Fresh; destruct H2;
+specialize H0 with x x0; apply H0 with (w':=x0) in H1; auto;
+apply lc_w_n_Hyb_subst_t in H1; apply lc_w_n_Hyb_subst_w in H1; auto.
+assert (exists x, x \notin L_t) by apply Fresh; destruct H2;
+assert (exists x, x \notin L_w) by apply Fresh; destruct H3;
+specialize H0 with x x0; apply H0 with (w':=x0) in H2; auto;
+apply lc_w_n_Hyb_subst_t in H2; apply lc_w_n_Hyb_subst_w in H2; auto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma subst_w_Hyb_comm2:
+forall M w w' m n
+  (Neq: m <> n)
+  (Neq': w <> bwo m),
+  {{fwo w'//bwo m }}({{w//bwo n}}M) =
+  {{w//bwo n}}({{fwo w'//bwo m}}M).
+induction M; intros; simpl; destruct w;
+repeat case_if; subst; simpl; auto;
+rewrite IHM || (rewrite IHM1; try rewrite IHM2);
+auto; intro nn; elim Neq'; inversion nn; subst; auto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma double_subst_w_Hyb_bwo:
+forall N w w' n,
+ w' <> bwo n ->
+ {{w // bwo n}}({{w' // bwo n}}N) = {{w' // bwo n}}N.
+induction N; destruct w'; simpl in *; intros; repeat case_if;
+try rewrite IHN;
+try (rewrite IHN1; try rewrite IHN2); auto; intro nn; inversion nn;
+subst; elim H; auto.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma subst_t_comm2_Hyb:
+forall M v' m n N
+  (Neq: m <> n)
+  (Lc: lc_t_Hyb N),
+  subst_t_Hyb N (bte m) (subst_t_Hyb (hyp_Hyb (fte v')) (bte n) M) =
+  subst_t_Hyb (hyp_Hyb (fte v')) (bte n) (subst_t_Hyb N (bte m) M).
+induction M; intros; subst; simpl;
+repeat (case_if; subst; simpl); auto;
+try rewrite IHM; eauto; try omega.
+rewrite closed_subst_t_Hyb_bound with (n:=0); auto; omega.
+rewrite IHM1; eauto; rewrite IHM2; eauto; omega.
+rewrite IHM1; eauto; rewrite IHM2; eauto; omega.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma subst_t_comm2_LF:
+forall M v' m n N
+  (Neq: m <> n)
+  (Lc: lc_t_LF N),
+  subst_t_LF N (bte m) (subst_t_LF (hyp_LF (fte v')) (bte n) M) =
+  subst_t_LF (hyp_LF (fte v')) (bte n) (subst_t_LF N (bte m) M).
+induction M; intros; subst; simpl;
+repeat (case_if; subst; simpl); auto;
+try rewrite IHM; eauto; try omega.
+rewrite closed_subst_t_bound_LF with (n:=0); auto; omega.
+rewrite IHM1; eauto; rewrite IHM2; eauto; omega.
+rewrite IHM1; eauto; rewrite IHM2; eauto; omega.
+Qed.
+
+(* FIXME: Move this to Hybrid/Hyb_Substitution *)
+Lemma reorder_subst_w_Hyb_eq:
+forall M w w' w'',
+  subst_w_Hyb w w' (subst_w_Hyb w w'' M) =
+  subst_w_Hyb w w'' (subst_w_Hyb w w' M).
+induction M; intros; simpl; repeat case_if;
+try rewrite IHM;
+try rewrite IHM1; try rewrite IHM2; eauto.
+Qed.
+
+Lemma LF_to_Hyb_subst_t:
+forall Gamma C2 C1 M1 M2 v A B G' w,
+  lc_t_LF C1 -> lc_t_Hyb C2 ->
+  LF_to_Hyb_rel Gamma M1 A G' w M2 ->
+  LF_to_Hyb_rel Gamma C1 B G' w C2 ->
+  LF_to_Hyb_rel Gamma (subst_t_LF C1 (bte v) M1) A G' w ([C2//(bte v)]M2).
+intros; generalize dependent C1; generalize dependent C2;
+generalize dependent B. generalize dependent v.
+induction H1; intros; simpl in *.
+repeat case_if; [inversion H | econstructor; eauto].
+econstructor; unfold open_LF in *; unfold open_t_Hyb in *.
+inversion H; subst; econstructor; eauto;
+intros; unfold open_LF in *; simpl.
+specialize H2 with v0 (S v) B C2 C1; apply H2 with (S v) B C2 C1 in H6; auto.
+rewrite <- subst_t_comm2_LF; try omega; auto.
+inversion H6; subst; auto.
+
+Lemma LF_to_Hyb_subst_t_rev:
+forall Gamma C2 C1 M1 M2 k A G' w,
+  LF_to_Hyb_rel Gamma (subst_t_LF C1 (bte k) M1) A G' w M2 ->
+  LF_to_Hyb_rel Gamma C1 A G' w C2 ->
+  exists M2',
+    M2 = subst_t_Hyb C2 (bte k) M2'.
+intros. induction H; simpl in *.
+(* hyp *)
+inversion H; subst; exists (hyp_Hyb (fte v0)); simpl; case_if; auto.
+(* lam *)
+assert (exists x, x \notin L) by apply Fresh; destruct H4.
+destruct H3 with x; auto.
+exists (lam_Hyb A
+
+Admitted.
+
+Lemma LF_to_Hyb_step:
+forall M G' Gamma A w M' N N',
+  LF_to_Hyb_rel Gamma M A G' w M' ->
+  LF_to_Hyb_rel Gamma N A G' w N' ->
+  step_LF M N ->
+  step_Hyb (M', fwo w) (N', fwo w).
+intros. generalize dependent N; generalize dependent N'.
+induction H; intros.
+inversion H2.
+inversion H4; subst.
+(* appl *)
+inversion H4; subst.
+inversion H0; subst.
+assert (LF_to_Hyb_rel Gamma (subst_t_LF N1 (bte 0) M) B G' w N') as NN by auto.
+apply LF_to_Hyb_subst_t_rev with (M1:=M) (M2:=N') (k:=0) in NN.
+
+
+
+
+
+(* ind: M
+induction M; intros; inversion H; inversion H1; subst.
+(* appl_lam *)
+inversion H5; subst.
+*)
+
 Close Scope hybrid_is5_scope.
 Close Scope permut_scope.
 Close Scope is5_scope.
