22f67a15eed1bdf8d5bd3636c716811ede1b6dbf
Agata M <ayertienna@gmail.com>
2013-04-15 02:09:16 +0200

    LF_to_Hyb - lemmas for typing: no admitted ones

--- LanguagesEquivalence/LF_to_Hyb.v
+++ LanguagesEquivalence/LF_to_Hyb.v
@@ -11,45 +11,112 @@ Require Import Setoid.
 
 Open Scope is5_scope.
 Open Scope permut_scope.
-(* FIXME: There is no labelfree is5 scope *)
 Open Scope hybrid_is5_scope.
 
 Definition LF_to_Hyb_ctx (G: bg_LF) (G': bg_Hyb) :=
-  map snd_ G' *=* G /\ ok_Hyb G' nil.
+  PPermut_LF (map snd_ G') G /\ ok_Hyb G' nil.
+
+Lemma ok_LF_ok_Hyb:
+forall (G: list (var*ty)) U,
+  ok_LF G U -> ok_Hyb G U.
+induction G; intros; try constructor; destruct a; inversion H; subst;
+constructor; auto.
+Qed.
 
 Lemma LF_to_Hyb_ctx_Ok:
-forall G G',
+forall G' G,
   ok_Bg_LF G -> LF_to_Hyb_ctx G G' -> ok_Bg_Hyb G'.
-Admitted.
+induction G'; intros; unfold ok_Bg_Hyb in *; unfold ok_Bg_LF in *;
+unfold LF_to_Hyb_ctx in *; split; destruct H0; auto; try constructor;
+rewrite <- flat_map_concat.
+apply ok_LF_PPermut with (G':=map snd_ (a::G')) in H.
+destruct a; rew_map in *; simpl in *; rew_concat in *; auto.
+apply ok_LF_ok_Hyb; auto.
+symmetry; auto.
+Qed.
 
 Lemma LF_to_Hyb_ctx_extend_t:
 forall G G' w Gamma v A,
 LF_to_Hyb_ctx (((v,A)::Gamma)::G) ((w, (v,A)::Gamma)::G') ->
 LF_to_Hyb_ctx (Gamma::G) ((w, Gamma)::G').
-Admitted.
+unfold LF_to_Hyb_ctx in *; intros; destruct H; split; rew_map in *;
+simpl in *.
+PPermut_LF_simpl.
+apply PPermut_LF_last_rev_simpl with (a:=((v,A)::Gamma));
+transitivity (((v,A)::Gamma) :: G).
+rewrite <- H; PPermut_LF_simpl. PPermut_LF_simpl.
+inversion H0; subst; constructor; auto.
+Qed.
 
 Lemma LF_to_Hyb_ctx_extend_w:
 forall G G' w,
   LF_to_Hyb_ctx (nil::G) ((w, nil)::G') ->
   LF_to_Hyb_ctx G G'.
-Admitted.
+unfold LF_to_Hyb_ctx in *; intros; destruct H; split; rew_map in *;
+simpl in *.
+apply PPermut_LF_last_rev_simpl with (a:=nil);
+transitivity (nil :: G).
+rewrite <- H; PPermut_LF_simpl. PPermut_LF_simpl.
+inversion H0; subst. apply ok_Hyb_used_weakening in H6; auto.
+Qed.
 
 Lemma LF_to_Hyb_ctx_extend2_t:
 forall G G' w Gamma A,
 LF_to_Hyb_ctx (Gamma::G) ((w, Gamma)::G') ->
 forall v, v \notin \{} ->
 LF_to_Hyb_ctx (((v,A)::Gamma)::G) ((w, (v,A)::Gamma)::G').
-Admitted.
+unfold LF_to_Hyb_ctx in *; intros; destruct H; split;
+rew_map in *; simpl in *.
+PPermut_LF_simpl.
+apply PPermut_LF_last_rev_simpl with (a:=Gamma);
+transitivity (Gamma :: G).
+rewrite <- H; symmetry; apply PPermut_LF_first_last.
+PPermut_LF_simpl.
+inversion H1; subst; constructor; auto.
+Qed.
 
 Lemma LF_to_Hyb_ctx_extend2_w:
 forall G G',
   LF_to_Hyb_ctx G G' ->
-  forall w, w \notin \{} ->
+  forall w, w \notin used_w_vars_Hyb G' ->
   LF_to_Hyb_ctx (nil::G) ((w, nil)::G').
-Admitted.
+unfold LF_to_Hyb_ctx in *; intros; destruct H; split;
+rew_map in *; simpl in *.
+PPermut_LF_simpl; auto.
+apply ok_Hyb_fresh_wo_list; auto.
+rewrite notin_union; rewrite from_list_nil; split; auto.
+Qed.
+
+Lemma LF_to_Hyb_PPermut_LF_bg:
+forall G G' G'',
+  LF_to_Hyb_ctx G G'' ->
+  PPermut_LF G G' ->
+  LF_to_Hyb_ctx G' G''.
+unfold LF_to_Hyb_ctx in *; intros; destruct H; split; auto.
+rewrite H; auto.
+Qed.
+
+Lemma LF_to_Hyb_PPermut_Hyb_bg:
+forall G' G'' G,
+  LF_to_Hyb_ctx G G' ->
+  PPermut_Hyb G' G'' ->
+  LF_to_Hyb_ctx G G''.
+unfold LF_to_Hyb_ctx in *; intros; destruct H; split.
+rewrite <- H.
+clear G H H1; symmetry; induction H0;
+auto;rew_map; simpl; PPermut_LF_simpl.
+rewrite H; rewrite H0; auto.
+rewrite IHPPermut_Hyb1; rewrite IHPPermut_Hyb2; auto.
+rewrite <- H0; auto.
+Qed.
 
 Add Morphism LF_to_Hyb_ctx: LF_to_Hyb_ctx_LF.
-Admitted.
+intros; split; intros.
+apply LF_to_Hyb_PPermut_LF_bg with (G:=x); auto;
+apply LF_to_Hyb_PPermut_Hyb_bg with (G':=x0); auto.
+apply LF_to_Hyb_PPermut_LF_bg with (G:=y); auto;
+apply LF_to_Hyb_PPermut_Hyb_bg with (G':=y0); auto.
+Qed.
 
 Fixpoint LF_to_Hyb_ctx_fun (G: bg_LF) (U: list var) :=
 match G with
@@ -59,15 +126,7 @@ match G with
   (w, Gamma) :: LF_to_Hyb_ctx_fun G' (w::U)
 end.
 
-(* For each good context from LF there is a context from Hyb that extends it *)
-(* Namely the one created in LF_to_Hyb_ctx_fun *)
-Lemma LF_to_Hyb_ctx_Exists:
-forall Gamma G,
-  ok_Bg_LF (Gamma::G) ->
-  exists w' G', LF_to_Hyb_ctx (Gamma::G) ((w', Gamma)::G').
-Admitted.
-
-Inductive LF_to_Hyb_rel: (* bg_LF  -> *) ctx_LF  -> te_LF  -> ty ->
+Inductive LF_to_Hyb_rel: ctx_LF  -> te_LF  -> ty ->
                          bg_Hyb -> var -> te_Hyb -> Prop :=
 
 | hyp_LF_Hyb:
@@ -114,14 +173,15 @@ Inductive LF_to_Hyb_rel: (* bg_LF  -> *) ctx_LF  -> te_LF  -> ty ->
                     G' w (unbox_fetch_Hyb (fwo w) N)
 
 | unbox_fetch_LF_Hyb:
-    forall G Gamma Gamma' G' w w' M N A,
+    forall G Gamma Gamma' G' w w' M N A G'',
       w <> w' ->
       types_LF (Gamma'::G) Gamma (unbox_LF M) A ->
       LF_to_Hyb_ctx (Gamma::Gamma'::G) ((w, Gamma)::(w', Gamma')::G') ->
       LF_to_Hyb_rel Gamma' M ([*]A)
                     ((w, Gamma)::G') w' N ->
+      ((w', Gamma')::G') ~=~ G'' ->
       LF_to_Hyb_rel Gamma (unbox_LF M) A
-                    ((w', Gamma')::G') w (unbox_fetch_Hyb (fwo w') N)
+                    G'' w (unbox_fetch_Hyb (fwo w') N)
 
 | here_LF_Hyb:
     forall G Gamma G' w A M N,
@@ -132,14 +192,15 @@ Inductive LF_to_Hyb_rel: (* bg_LF  -> *) ctx_LF  -> te_LF  -> ty ->
                     G' w (get_here_Hyb (fwo w) N)
 
 | get_here_LF_Hyb:
-    forall G Gamma Gamma' G' w w' A M N,
+    forall G Gamma Gamma' G' w w' A M N G'',
       w <> w' ->
       types_LF (Gamma'::G) Gamma (here_LF M) (<*>A) ->
       LF_to_Hyb_ctx (Gamma::Gamma'::G) ((w, Gamma)::(w', Gamma')::G') ->
       LF_to_Hyb_rel Gamma' M A
                     ((w, Gamma)::G') w' N ->
+      ((w', Gamma')::G') ~=~ G'' ->
       LF_to_Hyb_rel Gamma (here_LF M) (<*>A)
-                    ((w', Gamma')::G') w (get_here_Hyb (fwo w') N)
+                    G'' w (get_here_Hyb (fwo w') N)
 
 | letdia_LF_Hyb:
     forall Lt Lw G Gamma G' w A B M1 M2 N1 N2,
@@ -154,7 +215,7 @@ Inductive LF_to_Hyb_rel: (* bg_LF  -> *) ctx_LF  -> te_LF  -> ty ->
                     (letdia_get_Hyb (fwo w) M2 N2)
 
 | letdia_get_LF_Hyb:
-    forall Lt Lw G Gamma Gamma' G' w w' A B M1 M2 N1 N2,
+    forall Lt Lw G Gamma Gamma' G' w w' A B M1 M2 N1 N2 G'',
       types_LF (Gamma'::G) Gamma (letdia_LF M1 N1) B ->
       LF_to_Hyb_rel Gamma' M1 (<*>A)
                     ((w, Gamma)::G') w' M2 ->
@@ -163,33 +224,87 @@ Inductive LF_to_Hyb_rel: (* bg_LF  -> *) ctx_LF  -> te_LF  -> ty ->
                        (((w'0, (v', A) :: nil ) :: (w', Gamma')::G'))
                        w ((N2 ^w^ fwo w'0 ) ^t^ hyp_Hyb (fte v'))) ->
       LF_to_Hyb_ctx (Gamma::Gamma'::G) ((w, Gamma)::(w', Gamma')::G') ->
+      (w', Gamma')::G' ~=~ G'' ->
       LF_to_Hyb_rel Gamma (letdia_LF M1 N1) B
-                    ((w', Gamma')::G') w
+                    G'' w
                     (letdia_get_Hyb (fwo w') M2 N2)
 .
 
-Lemma LF_to_Hyb_rel_permut_G:
-forall Gamma M A G w M' G',
+Lemma LF_to_Hyb_rel_permut_G_Gamma:
+forall Gamma Gamma' M A G w M' G',
+  Gamma *=* Gamma' ->
   G ~=~ G' ->
   LF_to_Hyb_rel Gamma M A G w M' ->
-  LF_to_Hyb_rel Gamma M A G' w M'.
-intros; generalize dependent G'; induction H0; intros; try econstructor; eauto;
-try rewrite <- H1 || rewrite <- H2 || rewrite <- H3; auto.
-Admitted.
-
-Lemma LF_to_Hyb_rel_permut_Gamma:
-forall Gamma M A G w M' Gamma',
-  Gamma *=* Gamma' ->
-  LF_to_Hyb_rel Gamma M A G w M' ->
-  LF_to_Hyb_rel Gamma' M A G w M'.
-Admitted.
+  LF_to_Hyb_rel Gamma' M A G' w M'.
+intros; generalize dependent G'; generalize dependent Gamma';
+induction H1; intros; try econstructor; eauto.
+rewrite <- H1; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by (rewrite H1; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H2; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H3; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by (rewrite H3; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H4; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H1; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by (rewrite H1; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H2; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H3; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by (rewrite H3; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H4; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H2; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by (rewrite H2; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H3; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H4; eauto.
+assert (PPermut_LF (Gamma::Gamma'::G) (Gamma'0::Gamma'::G)) as HP1 by
+  (rewrite H4; auto);
+assert (((w, Gamma) :: (w', Gamma') :: G') ~=~
+  ((w, Gamma'0):: (w', Gamma') :: G')) as HP2 by
+    (rewrite H3; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H5; rewrite H3; auto.
+rewrite <- H2; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by (rewrite H2; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H3; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H4; eauto.
+assert (PPermut_LF (Gamma::Gamma'::G) (Gamma'0::Gamma'::G)) as HP1 by
+  (rewrite H4; auto);
+assert (((w, Gamma) :: (w', Gamma') :: G') ~=~
+  ((w, Gamma'0):: (w', Gamma') :: G')) as HP2 by
+    (rewrite H3; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H5; rewrite H3; auto.
+rewrite <- H4; eauto.
+assert (PPermut_LF (Gamma::G) (Gamma'::G)) as HP1 by
+  (rewrite H4; auto);
+assert (((w, Gamma) :: G') ~=~ ((w, Gamma') :: G'0)) as HP2 by
+    (rewrite H5; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H5; eauto.
+assert (PPermut_LF (Gamma::Gamma'::G) (Gamma'0::Gamma'::G)) as HP1 by
+  (rewrite H5; auto);
+assert (((w, Gamma) :: (w', Gamma') :: G') ~=~
+  ((w, Gamma'0):: (w', Gamma') :: G')) as HP2 by
+    (rewrite H4; PPermut_Hyb_simpl);
+rewrite <- HP1; rewrite <- HP2; eauto.
+rewrite <- H6; rewrite H4; auto.
+Qed.
 
 Add Morphism LF_to_Hyb_rel: LF_to_Hyb_rel_G.
 split; intros.
-apply LF_to_Hyb_rel_permut_G with (G:=x0); auto;
-apply LF_to_Hyb_rel_permut_Gamma with (Gamma:=x); auto.
-apply LF_to_Hyb_rel_permut_G with (G:=y2); auto;
-apply LF_to_Hyb_rel_permut_Gamma with (Gamma:=y); auto; symmetry; auto.
+eapply LF_to_Hyb_rel_permut_G_Gamma with (G:=x0) (Gamma:=x); auto.
+eapply LF_to_Hyb_rel_permut_G_Gamma with (G:=y2) (Gamma:=y); auto; symmetry;
+auto.
 Qed.
 
 Lemma LF_to_Hyb_types:
@@ -221,24 +336,25 @@ assert (PPermut_LF (Gamma0 :: G0 & Gamma) (Gamma::G))
        by (rewrite <- H6; PPermut_LF_simpl);
 rewrite H2; auto.
 (* unbox-fetch *)
+rewrite <- H3;
 inversion H0; subst.
 apply t_unbox_fetch_Hyb with (G:=G') (Gamma:=Gamma').
 assert ((w, Gamma) :: (w', Gamma') :: G' ~=~ (w', Gamma') :: G' & (w, Gamma))
   by PPermut_Hyb_simpl;
-rewrite <- H3; eapply LF_to_Hyb_ctx_Ok ; eauto.
+rewrite <- H4; eapply LF_to_Hyb_ctx_Ok ; eauto.
 assert (G' & (w, Gamma) ~=~ (w, Gamma) :: G') by PPermut_Hyb_simpl;
-rewrite H3; auto.
+rewrite H4; auto.
 PPermut_Hyb_simpl.
 apply t_unbox_fetch_Hyb with (G:=G') (Gamma:=Gamma').
 assert ((w, Gamma) :: (w', Gamma') :: G' ~=~ (w', Gamma') :: G' & (w, Gamma))
   by PPermut_Hyb_simpl.
 assert (PPermut_LF (Gamma0 :: G0 & Gamma) (Gamma :: Gamma' :: G)) by
-(transitivity (G0 & Gamma0 & Gamma); [ | rewrite H7 ];
+(transitivity (G0 & Gamma0 & Gamma); [ | rewrite H8 ];
  rew_app; PPermut_LF_simpl).
-rewrite H5 in Ok.
-rewrite <- H3; eapply LF_to_Hyb_ctx_Ok ; eauto.
+rewrite H6 in Ok.
+rewrite <- H4; eapply LF_to_Hyb_ctx_Ok ; eauto.
 assert (G' & (w, Gamma) ~=~ (w, Gamma) :: G') by PPermut_Hyb_simpl;
-rewrite H3; auto.
+rewrite H4; auto.
 PPermut_Hyb_simpl.
 (* here *)
 inversion H; subst.
@@ -249,24 +365,24 @@ assert (PPermut_LF (Gamma0 :: G0 & Gamma) (Gamma::G))
        by (rewrite <- H7; PPermut_LF_simpl);
 rewrite H2; auto.
 (* get-here *)
-inversion H0; subst.
+rewrite <- H3; inversion H0; subst.
 apply t_get_here_Hyb with (G:=G') (Gamma:=Gamma').
 assert ((w, Gamma) :: (w', Gamma') :: G' ~=~ (w', Gamma') :: G' & (w, Gamma))
   by PPermut_Hyb_simpl;
-rewrite <- H3; eapply LF_to_Hyb_ctx_Ok ; eauto.
+rewrite <- H4; eapply LF_to_Hyb_ctx_Ok ; eauto.
 assert (G' & (w, Gamma) ~=~ (w, Gamma) :: G') by PPermut_Hyb_simpl;
-rewrite H3; auto.
+rewrite H4; auto.
 PPermut_Hyb_simpl.
 apply t_get_here_Hyb with (G:=G') (Gamma:=Gamma').
 assert ((w, Gamma) :: (w', Gamma') :: G' ~=~ (w', Gamma') :: G' & (w, Gamma))
   by PPermut_Hyb_simpl.
 assert (PPermut_LF (Gamma0 :: G0 & Gamma) (Gamma :: Gamma' :: G)) by
-(transitivity (G0 & Gamma0 & Gamma); [ | rewrite H8 ];
+(transitivity (G0 & Gamma0 & Gamma); [ | rewrite H9 ];
  rew_app; PPermut_LF_simpl).
-rewrite H4 in Ok.
-rewrite <- H3; eapply LF_to_Hyb_ctx_Ok ; eauto.
+rewrite H5 in Ok.
+rewrite <- H4; eapply LF_to_Hyb_ctx_Ok ; eauto.
 assert (G' & (w, Gamma) ~=~ (w, Gamma) :: G') by PPermut_Hyb_simpl;
-rewrite H3; auto.
+rewrite H4; auto.
 PPermut_Hyb_simpl.
 (* letdia *)
 inversion H; subst.
@@ -277,32 +393,32 @@ assert (PPermut_LF (Gamma0 :: G0 & Gamma) (Gamma::G))
        by (rewrite <- H9; PPermut_LF_simpl);
 rewrite H4; auto.
 (* letdia-get *)
-inversion H; subst.
+rewrite <- H4; inversion H; subst.
 eapply t_letdia_get_Hyb with (G:=G') (Gamma:=Gamma').
 assert ((w, Gamma) :: (w', Gamma') :: G' ~=~ (w', Gamma') :: G' & (w, Gamma))
   by PPermut_Hyb_simpl;
-rewrite <- H4; eapply LF_to_Hyb_ctx_Ok ; eauto.
+rewrite <- H5; eapply LF_to_Hyb_ctx_Ok ; eauto.
 assert (G' & (w, Gamma) ~=~ (w, Gamma) :: G') by PPermut_Hyb_simpl;
-rewrite H4; eauto.
+rewrite H5; eauto.
 intros;
 assert ((w'0, (v', A) :: nil) :: (w', Gamma') :: G' ~=~
         (w'0, (v', A) :: nil) :: G' & (w', Gamma')) by PPermut_Hyb_simpl;
-rewrite <- H6; eauto.
+rewrite <- H7; eauto.
 PPermut_Hyb_simpl.
 eapply t_letdia_get_Hyb with (G:=G') (Gamma:=Gamma').
 assert ((w, Gamma) :: (w', Gamma') :: G' ~=~ (w', Gamma') :: G' & (w, Gamma))
   by PPermut_Hyb_simpl.
 assert (PPermut_LF (Gamma0 :: G0 & Gamma) (Gamma :: Gamma' :: G)) by
-(transitivity (G0 & Gamma0 & Gamma); [ | rewrite H9 ];
+(transitivity (G0 & Gamma0 & Gamma); [ | rewrite H10 ];
  rew_app; PPermut_LF_simpl).
-rewrite H5 in Ok.
-rewrite <- H4; eapply LF_to_Hyb_ctx_Ok ; eauto.
+rewrite H6 in Ok.
+rewrite <- H5; eapply LF_to_Hyb_ctx_Ok ; eauto.
 assert (G' & (w, Gamma) ~=~ (w, Gamma) :: G') by PPermut_Hyb_simpl;
-rewrite H4; eauto.
+rewrite H5; eauto.
 intros;
 assert ((w'0, (v', A) :: nil) :: (w', Gamma') :: G' ~=~
         (w'0, (v', A) :: nil) :: G' & (w', Gamma')) by PPermut_Hyb_simpl;
-rewrite <- H6; eauto.
+rewrite <- H7; eauto.
 PPermut_Hyb_simpl.
 Qed.
 
@@ -406,14 +522,14 @@ apply R_subst_t_rev with (v:=0) (v':=x0); eauto.
 apply R_subst_w with (w:=fwo x) (w':=bwo 0); auto.
 (* letdia - get *)
 assert (exists x, x\notin Lw) by apply Fresh.
-destruct H4.
+destruct H5.
 assert (exists x0, x0 \notin Lt \u used_vars_te_LF N1 \u
                               free_vars_Hyb {{fwo x // bwo 0}}N2)
-  by apply Fresh; destruct H5.
+  by apply Fresh; destruct H6.
 specialize H2 with x x0.
 unfold open_LF in *; unfold open_t_Hyb in *;
 unfold open_w_Hyb in *.
-apply H2 in H4; auto.
+apply H2 in H5; auto.
 assert (R N1 ({{fwo x // bwo 0}} N2)).
 apply R_subst_t_rev with (v:=0) (v':=x0); eauto.
 apply R_subst_w with (w:=fwo x) (w':=bwo 0); auto.
